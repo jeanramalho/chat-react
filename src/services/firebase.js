9bab03d265d0a6ca5ffe0bedba4020eb64f7a15d
jeanramalho <jeanramalho.dev@gmail.com>
2022-08-15 14:15:23 -0300

    Implementa configurações do firebase

---
+++ src/services/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/compat/app"
+import "firebase/compat/auth"
+import "firebase/compat/firestore"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDfWpKRoZtHxDk5vQ09iY1v7MN6AJag53s",
+    authDomain: "chatapp-4db13.firebaseapp.com",
+    projectId: "chatapp-4db13",
+    storageBucket: "chatapp-4db13.appspot.com",
+    messagingSenderId: "933761194934",
+    appId: "1:933761194934:web:bf849b9cbaeb52e63cff17"
+  }
+
+  const app = firebase.initializeApp(firebaseConfig)
+  
+  const db = app.firestore()
+  const auth = app.auth()
+  const provider = new firebase.auth.GoogleAuthProvider()
+
+  export {db, auth, provider}
