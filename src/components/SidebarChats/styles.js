478946e7a76eff6683ef956ab4d00c3406db31c8
jeanramalho <jeanramalho.dev@gmail.com>
2022-08-17 12:05:40 -0300

    Inicia implementação de layout dos chats na barra lateral'

---
+++ src/components/SidebarChats/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    height: calc(100vh - 70px);
+    overflow-y: auto;
+
+    &::-webkit-scrollbar {
+        width: 6px;
+        border-radius: 10px;
+    }
+
+    &::-webkit-scrollbar-thumb {
+        border-radius: 10px;
+        background-color: rgba(0, 0, 0, 0.2)
+    }
+`
